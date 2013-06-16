2024298456154862d11d723fd6ec7593a0b93c43
bashtian <bashtian86@gmail.com>
2013-06-16 15:15:28 +0200

    updated readme and improved the CLI

--- README.md
+++ README.md
@@ -3,10 +3,20 @@ jsonutils
 
 Converter for JSON data to a Go struct or a Java class for GSON
 
-	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+	go get github.com/bashtian/jsonutils
 
-	f, err := jsonutils.ParseJson(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	jsonutils.PrintGo(f)
+You can print the structure of a JSON from a URL
+
+	jsonutils https://api.github.com/users/bashtian/repos
+
+or from a file
+
+	jsonutils -f file.json
+
+If you want to print example data as comments, use the -x parmeter
+
+	jsonutils -x https://api.github.com/users/bashtian/repos
+
+You can also print Java code 
+
+	jsonutils -j https://api.github.com/users/bashtian/repos
--- jsonutils/main.go
+++ jsonutils/main.go
@@ -9,10 +9,14 @@ import (
 )
 
 var filename = flag.String("f", "", "use a file as input")
+var withExamples = flag.Bool("x", false, "print examples as comment")
+var asJava = flag.Bool("j", false, "print Java instead of Go code")
 
 func main() {
 	flag.Parse()
 	url := flag.Arg(0)
+
+	var m *jsonutils.Model
 
 	if *filename != "" {
 		b, err := ioutil.ReadFile(*filename)
@@ -25,16 +29,21 @@ func main() {
 		}
 		s := strings.Split(*filename, ".")
 
-		m := jsonutils.New(f, s[0])
-		m.WriteGo()
+		m = jsonutils.New(f, s[0])
 	} else if url != "" {
-		m, err := jsonutils.GetModel(flag.Arg(0))
+		var err error
+		m, err = jsonutils.GetModel(url)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		m.WriteGo()
 	} else {
 		log.Fatal("missing URL")
 	}
 
+	m.WithExample = *withExamples
+	if *asJava {
+		m.WriteJava()
+	} else {
+		m.WriteGo()
+	}
 }
