7f7e28da2a99402d337aa0a763ce1097cc5c7bfc
bashtian <bashtian86@gmail.com>
2015-01-08 16:11:33 +0100

    Sort keys by ID first

--- jsonutils.go
+++ jsonutils.go
@@ -411,9 +411,22 @@ func getSortedKeys(m map[string]interface{}) (keys []string) {
 	for key := range m {
 		keys = append(keys, key)
 	}
-	sort.Strings(keys)
+	sort.Sort(ByIDFirst(keys))
 	return
 }
+
+type ByIDFirst []string
+
+func (p ByIDFirst) Len() int { return len(p) }
+func (p ByIDFirst) Less(i, j int) bool {
+	if p[i] == "id" {
+		return true
+	} else if p[j] == "id" {
+		return false
+	}
+	return p[i] < p[j]
+}
+func (p ByIDFirst) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 
 func getName(u string) string {
 	p, err := url.Parse(u)
